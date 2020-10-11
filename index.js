4774e62a1c5a83bf8fb00b1ca401a40054433b81
Karen Deng <karen.deng@duke.edu>
2020-10-11 19:50:21 -0400

    finished frontend of submit page

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var path = require('path');
 //   res.sendFile(path.join(__dirname+'/home.html'));
 // })
 
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/views/home.html'));
   //__dirname : It will resolve to your project folder.
