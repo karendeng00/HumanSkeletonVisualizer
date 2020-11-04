93166a907f97b0507817cd2436514c4b827c31a4
Karen Deng <karen.deng@duke.edu>
2020-11-04 11:13:33 -0500

    can zoom in

--- public/javascripts/visualizeData.js
+++ public/javascripts/visualizeData.js
@@ -2,32 +2,50 @@ import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threej
 import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js';
 import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/loaders/GLTFLoader.js';
 
-window.onload = function() {
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+let camera, scene, renderer;
 
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+const mouse = new THREE.Vector2();
+const target = new THREE.Vector2();
+const windowHalf = new THREE.Vector2( window.innerWidth / 2, window.innerHeight / 2 );
 
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.background = new THREE.Color( 0xADD8E6);
-   
+init();
+animate();
+
+function init() {
+
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 500 );
+    
     camera.position.z = 1;
     camera.position.y = 1;
+
+    scene = new THREE.Scene();
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    scene.background = new THREE.Color( 0xADD8E6);
+    document.body.appendChild( renderer.domElement );
+
     var loader = new GLTFLoader();
     loader.load("../blender/manikin.glb", gltf => {
-        
-        scene.add( gltf.scene );
-        renderer.render(scene, camera);
-        console.log(renderer)
-
+    scene.add( gltf.scene );
+    // renderer.render(scene, camera);
     }, undefined, function ( error ) {
         console.error( error );
     }
     );
+    
+    document.addEventListener( 'wheel', onMouseWheel, false );
+    
+
+    
+}
+// var scene = new THREE.Scene();
+// var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+
     // var data = JSON.parse(localStorage.getItem('data'));
     // var acc_data = ["RKN^", "HIP", "LUA^", "RUA_", "LH", "BACK", "RKN_", "RWR", "RUA^", "LUA_", "LWR", "RH"];
     // var inertial_data = ["BACK", "RUA", "RLA", "LUA", "LLA"];
@@ -51,4 +69,24 @@ window.onload = function() {
     //     document.getElementById('table').innerHTML +="<br>"
     // }
     
-}   
+
+
+
+function onMouseWheel( event ) {
+
+    camera.position.z += event.deltaY * 0.1; // move camera along z-axis
+  
+}
+
+function animate() {
+
+    target.x = ( 1 - mouse.x ) * 0.002;
+    target.y = ( 1 - mouse.y ) * 0.002;
+  
+    camera.rotation.x += 0.05 * ( target.y - camera.rotation.x );
+    camera.rotation.y += 0.05 * ( target.x - camera.rotation.y );
+
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+
+}
